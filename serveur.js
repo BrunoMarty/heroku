04d5eeb8457f9daf090e35bdf5ab2fddc0b044f0
bruno <bruno.marty4@gmail.com>
2017-04-13 12:33:08 +0200

    mise en place du fw express

--- serveur.js
+++ serveur.js
@@ -1,14 +1,28 @@
-var http = require('http');
+var express = require('express');
+var serveur = express();
 
-var hostname = '127.0.0.1';
-var port = 12107;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+serveur.get("/",function(request,response){
+    response.sendFile(__dirname +"/views/index.html");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+serveur.get("/test",function(request,response){
+    response.send("Test");
 });
+
+serveur.post("/test",function(request,response){
+    response.send("Test en post");
+});
+
+serveur.listen(12107);
+// var hostname = '127.0.0.1';
+// var port = 12107;
+//
+// const server = http.createServer(function(request,response) {
+//   response.statusCode = 200;
+//   response.setHeader('Content-Type', 'text/plain');
+//   response.end('Hello World\n');
+// });
+//
+// server.listen(port, ip, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
