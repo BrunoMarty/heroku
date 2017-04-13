3234710ef195562de9a6e80ae50213893a48c382
bruno <bruno.marty4@gmail.com>
2017-04-13 11:38:04 +0200

    Initialisation du serveur

---
+++ serveur.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 12107;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
